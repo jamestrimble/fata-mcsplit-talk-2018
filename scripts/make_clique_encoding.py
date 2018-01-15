cb77cb3d66f921131ec709aba8c8fbf968751d30
James Trimble <james.trimble@yahoo.co.uk>
2018-01-15 13:54:25 +0000

    More writing

--- scripts/make_clique_encoding.py
+++ scripts/make_clique_encoding.py
@@ -1,9 +1,10 @@
 import math
 
-ee0_str = "1--4; 1--5; 2--3; 2--5; 3--5;"
-ee1_str = "a--b; a--c; a--e; b--d; b--f; c--d; c--e; c--f; d--f; e--f;"
-vv0 = range(5)
-vv1 = range(6)
+ee0_str = "1--4; 2--3; 2--4; 3--4;"
+ee1_str = "a--b; b--c; c--d; d--a;"
+vv0 = range(4)
+vv1 = range(4)
+clique_nodes = [(0,0), (3,3), (2,2)]
 
 def read_edges(ee_str, start_char):
     start_char_code = ord(start_char)
@@ -15,10 +16,10 @@ ee0 = read_edges(ee0_str, "1")
 ee1 = read_edges(ee1_str, "a")
 
 start_angles = [2 * math.pi * i / len(vv0) for i in vv0]
-end_angles = [2 * math.pi * (i+0.75) / len(vv0) for i in vv0]
+end_angles = [2 * math.pi * (i+0.65) / len(vv0) for i in vv0]
 
 def position(angle):
-    angle = .655 * math.pi - angle
+    angle = .4 * math.pi - angle
     radius = 2
     return (radius * math.cos(angle), radius * math.sin(angle))
 
@@ -37,24 +38,29 @@ def print_nodes():
             print "\\node[draw, circle, fill=white, inner sep=.3pt] (N{}{}) at ({},{}) {{\\scriptsize {}{}}};".format(
                     i+1, letter, x, y, i+1, letter)
 
-def print_edges():
+def print_edges(bold_edges):
     for i in vv0:
         for j in vv1:
             for k in vv0:
                 for l in vv1:
                     if k>i:
-                        letter_j = "abcdef"[j]
-                        letter_l = "abcdef"[l]
-                        if (set([i,k]) in ee0) == (set([j,l]) in ee1):
-                            print "\\draw [lledge] (N{}{}) -- (N{}{});".format(
-                                    i+1, letter_j, k+1, letter_l)
+                        if not bold_edges or (i,j) in clique_nodes and (k,l) in clique_nodes:
+                            letter_j = "abcdef"[j]
+                            letter_l = "abcdef"[l]
+                            edge_type = "edge" if bold_edges else "lledge"
+                            if (set([i,k]) in ee0) == (set([j,l]) in ee1):
+                                print "\\draw [{}] (N{}{}) -- (N{}{});".format(
+                                        edge_type, i+1, letter_j, k+1, letter_l)
 
 print "\\vspace{-.5cm}"
+print "\\visible<2-> {"
 print "\\begin{center}\\begin{tikzpicture}[scale=1.15]"
 
 print_nodes()
-print_edges()
-print ee0
-print ee1
+print_edges(False)
+print "\\visible<3> {"
+print_edges(True)
+print "}"
+print "}"
 
 print "\\end{tikzpicture}\\end{center}"
